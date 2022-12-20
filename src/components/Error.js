19aaaf1c31f272c88d470dd5c4430531143a7756
sayeed <sayeed0209@gmail.com>
2022-12-20 17:02:37 +0100

    error component

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Error = () => {
+  return (
+    <div className=" text-center mt-5">
+      <h2 id="my-test-id">Oppps! there was an error...</h2>
+    </div>
+  );
+};
+
+export default Error;

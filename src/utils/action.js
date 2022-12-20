c45853bc4c1cd0f8ea58bfab1a078ba4404f121e
sayeed <sayeed0209@gmail.com>
2022-12-20 16:50:20 +0100

    initial project setup

---
+++ src/utils/action.js
@@ -0,0 +1,13 @@
+export const GET_PODCAST_BEGIN = "GET_PODCAST_BEGIN";
+export const GET_PODCAST_SUCCESS = "GET_PODCAST_SUCCESS";
+export const GET_PODCAST_ERROR = "GET_PODCAST_ERROR";
+export const GET_SINGLE_PODCAST_BEGIN = "GET_SINGLE_PODCAST_BEGIN";
+export const GET_SINGLE_PODCAST_SUCCESS = "GET_SINGLE_PODCAST_SUCCESS";
+export const GET_SINGLE_PODCAST_ERROR = "GET_SINGLE_PRODUCT_ERROR";
+export const UPDATE_FILTERS = "UPDATE_FILTERS";
+export const LOAD_PODCAST = "LOAD_PODCAST";
+export const FILTER_PODCAST = "FILTER_PODCAST";
+export const CLEAR_FILTERS = "CLEAR_FILTERS";
+export const API_URL = "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json";
+export const SINGLE_PODCAST_URL =
+  "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=1535809341";
